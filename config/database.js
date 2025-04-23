d27a6f2181c8252bf0694ef78faef165267caf52
Fawwazaza <fawazrizakarnp@gmail.com>
2025-04-23 18:31:00 +0700

    nambah cloudbuild yaml

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize('notes', 'root','',{
-    host: 'localhost',
+const db = new Sequelize('notes', 'admin','password_db',{
+    host: '34.42.36.6',
     dialect : 'mysql'
 })
 
