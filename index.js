d48973511636ff8e33dedd2158d3c8ce2716201d
Fawwazaza <fawazrizakarnp@gmail.com>
2025-04-23 19:19:11 +0700

    memperbaiki permasalahan root endpoint

--- index.js
+++ index.js
@@ -7,4 +7,9 @@ app.use(cors());
 app.use(express.json());
 app.use(route);
 
-app.listen(5000, ()=> console.log('Server up and running....'));
+// Tambahkan ini
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+
+app.listen(5000, () => console.log("Server up and running...."));
